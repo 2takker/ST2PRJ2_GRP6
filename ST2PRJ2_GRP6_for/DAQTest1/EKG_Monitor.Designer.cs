fd818201f0ce97c2b6a5b1272890c5ad2fa4365d
2takker <2takker@gmail.com>
2017-03-13 12:29:16 +0100

    Add files via upload

---
+++ ST2PRJ2_GRP6_for/DAQTest1/EKG_Monitor.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace ST2PRJ2_GRP6_for
+{
+    partial class EKG_Monitor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.btnReadEKG = new System.Windows.Forms.Button();
+            this.chartEKG = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.txtBoxPulse = new System.Windows.Forms.TextBox();
+            this.txtBoxHRV = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chartEKG)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnReadEKG
+            // 
+            this.btnReadEKG.Location = new System.Drawing.Point(40, 27);
+            this.btnReadEKG.Name = "btnReadEKG";
+            this.btnReadEKG.Size = new System.Drawing.Size(231, 62);
+            this.btnReadEKG.TabIndex = 0;
+            this.btnReadEKG.Text = "Indlæs EKG";
+            this.btnReadEKG.UseVisualStyleBackColor = true;
+            this.btnReadEKG.Click += new System.EventHandler(this.btnReadEKG_Click);
+            // 
+            // chartEKG
+            // 
+            this.chartEKG.BackColor = System.Drawing.Color.WhiteSmoke;
+            chartArea1.AxisX.Interval = 1D;
+            chartArea1.AxisX.MajorGrid.Interval = 0.2D;
+            chartArea1.AxisX.MajorGrid.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Auto;
+            chartArea1.AxisX.MajorGrid.LineWidth = 2;
+            chartArea1.AxisX.Maximum = 10.6D;
+            chartArea1.AxisX.Minimum = 0D;
+            chartArea1.AxisX.MinorGrid.Enabled = true;
+            chartArea1.AxisX.MinorGrid.Interval = 0.04D;
+            chartArea1.AxisX.ScaleView.Size = 2.2D;
+            chartArea1.AxisX.ScrollBar.ButtonStyle = System.Windows.Forms.DataVisualization.Charting.ScrollBarButtonStyles.SmallScroll;
+            chartArea1.AxisX.ScrollBar.IsPositionedInside = false;
+            chartArea1.AxisX.ScrollBar.Size = 20D;
+            chartArea1.AxisX.Title = "s";
+            chartArea1.AxisX2.MinorGrid.Enabled = true;
+            chartArea1.AxisX2.MinorGrid.Interval = 20D;
+            chartArea1.AxisY.Interval = 0.5D;
+            chartArea1.AxisY.LineColor = System.Drawing.Color.White;
+            chartArea1.AxisY.MajorGrid.Interval = 0.5D;
+            chartArea1.AxisY.MajorGrid.LineWidth = 2;
+            chartArea1.AxisY.Maximum = 2D;
+            chartArea1.AxisY.Minimum = -0.5D;
+            chartArea1.AxisY.MinorGrid.Enabled = true;
+            chartArea1.AxisY.MinorGrid.Interval = 0.1D;
+            chartArea1.AxisY.Title = "mV";
+            chartArea1.AxisY2.MinorGrid.Enabled = true;
+            chartArea1.AxisY2.MinorGrid.Interval = 0.1D;
+            chartArea1.BackColor = System.Drawing.Color.WhiteSmoke;
+            chartArea1.Name = "ChartArea1";
+            this.chartEKG.ChartAreas.Add(chartArea1);
+            legend1.BackColor = System.Drawing.Color.WhiteSmoke;
+            legend1.Docking = System.Windows.Forms.DataVisualization.Charting.Docking.Top;
+            legend1.Name = "Legend1";
+            this.chartEKG.Legends.Add(legend1);
+            this.chartEKG.Location = new System.Drawing.Point(40, 119);
+            this.chartEKG.Name = "chartEKG";
+            series1.BorderWidth = 3;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Color = System.Drawing.Color.Maroon;
+            series1.Legend = "Legend1";
+            series1.Name = "EKG";
+            this.chartEKG.Series.Add(series1);
+            this.chartEKG.Size = new System.Drawing.Size(2642, 1329);
+            this.chartEKG.TabIndex = 2;
+            this.chartEKG.Text = "chart1";
+            // 
+            // txtBoxPulse
+            // 
+            this.txtBoxPulse.Location = new System.Drawing.Point(363, 58);
+            this.txtBoxPulse.Name = "txtBoxPulse";
+            this.txtBoxPulse.Size = new System.Drawing.Size(192, 31);
+            this.txtBoxPulse.TabIndex = 3;
+            this.txtBoxPulse.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtBoxHRV
+            // 
+            this.txtBoxHRV.Location = new System.Drawing.Point(739, 58);
+            this.txtBoxHRV.Name = "txtBoxHRV";
+            this.txtBoxHRV.Size = new System.Drawing.Size(192, 31);
+            this.txtBoxHRV.TabIndex = 4;
+            this.txtBoxHRV.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(363, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Puls";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(739, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "HRV";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(938, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 25);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "ms";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(561, 64);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 25);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "slag/min";
+            // 
+            // EKG_Monitor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2724, 1662);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBoxHRV);
+            this.Controls.Add(this.txtBoxPulse);
+            this.Controls.Add(this.chartEKG);
+            this.Controls.Add(this.btnReadEKG);
+            this.Name = "EKG_Monitor";
+            this.Text = "EKG Monitor";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.chartEKG)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnReadEKG;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartEKG;
+        private System.Windows.Forms.TextBox txtBoxPulse;
+        private System.Windows.Forms.TextBox txtBoxHRV;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ ST2PRJ2_GRP6_for/DAQTest1/EKG_Monitor.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ST2Prj2LibNI_DAQ;
+
+namespace ST2PRJ2_GRP6_for
+{
+    public partial class EKG_Monitor : Form
+    {
+        NI_DAQVoltage dataCollector;
+        List<double> dataListe;
+
+        public EKG_Monitor()
+        {
+            InitializeComponent();
+            dataListe = new List<double>();
+
+            dataCollector = new NI_DAQVoltage();
+            dataCollector.rangeMaximumVolt = 5;
+            dataCollector.sampleRateInHz = 500;
+            dataCollector.samplesPerChannel = 5000;
+        }
+
+        private void btnReadEKG_Click(object sender, EventArgs e)
+        {
+            btnReadEKG.Enabled = false;
+
+            dataListe.Clear();
+            chartEKG.Series["EKG"].Points.Clear();
+
+            double x = 0;
+
+            for (int i = 0; i < 100; i++)
+            {
+                dataListe.Add(0.0);
+            }
+
+            for (int i = 0; i < 100; i++)
+            {
+                dataListe.Add(1.0);
+            }
+
+            for (int i = 0; i < 100; i++)
+            {
+                dataListe.Add(0.0);
+            }
+
+            dataCollector.getVoltageSeqBlocking();
+
+            foreach(var ele in dataCollector.currentVoltageSeq)
+            {
+                dataListe.Add(ele);
+            }            
+
+            foreach (var ele in dataListe)
+            {
+                double y = ele;
+                chartEKG.Series["EKG"].Points.AddXY(x, y);
+                x += 0.002;
+            }
+
+            txtBoxPulse.Text = Convert.ToString(findPuls(dataListe));
+
+            txtBoxHRV.Text = Convert.ToString(findHRV(dataListe));
+
+            btnReadEKG.Enabled = true;
+
+            return;
+        }
+
+        private List<double> findToppunkt(List<double> inputListe)
+        {
+            List<double> toppunktsListe = new List<double>();
+            double maxValue = 0;
+
+
+            List<double> last10 = new List<double>();
+            for(int i = 0; i < inputListe.Count; i++)
+            {
+                last10.Add(inputListe[i]);
+                if (last10.Count > 10)
+                {
+                    last10.RemoveAt(0);
+                }
+
+                if (overLimit(last10, 1.0))
+                {
+                    if (inputListe[i] >= maxValue)
+                    {
+                        maxValue = inputListe[i];
+                    }
+                }
+                else
+                {
+                    if (maxValue > 0)
+                    {
+                        toppunktsListe.Add(i);
+                        maxValue = 0;
+                    }
+                }
+            }                  
+
+            return toppunktsListe;
+        }
+
+        private bool overLimit(List<double> values, double limit)
+        {
+            double sum = 0;
+            if (values.Count < 10)
+            {
+                return false;
+            }
+
+            foreach (var item in values)
+            {
+                sum += item;
+            }
+
+            double avg = (sum / values.Count);
+
+            if (avg > limit)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        private int findPuls(List<double> inputListe)
+        {
+            int puls = 0;
+            int rTakker = findToppunkt(inputListe).Count;
+
+            puls = rTakker * (60 / (dataCollector.samplesPerChannel / (int)dataCollector.sampleRateInHz));
+
+            return puls;
+        }
+
+        private long findHRV(List<double> inputListe)
+        {
+            long HRV = 0;
+            List<double> toppunktsListeX = findToppunkt(inputListe);            
+            double sum = 0;            
+
+            for (int i = 0; i < (toppunktsListeX.Count - 1); i++)
+            {
+                sum += (toppunktsListeX[i + 1] - toppunktsListeX[i]);
+            }
+
+            if (toppunktsListeX.Count > 0)
+            {
+                HRV = Convert.ToInt64(((sum / toppunktsListeX.Count)/500)*1000);
+            }
+            else
+            {
+                MessageBox.Show("Fejl, ingen signal ved elektroder");
+            }
+
+            return HRV;
+        }
+
+
+    }
+}
